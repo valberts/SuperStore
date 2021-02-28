66110715d3c73a5b2e03f1ca768c4c98cfbb8e5d
asakusa <albertsson.vincent@gmail.com>
2021-02-28 21:15:08 +0100

    Load data from the  route

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,48 @@
 import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react'
 
 function Home() {
-  return (
-    <div className="home-container">
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    fetch("https://gp-super-store-api.herokuapp.com/item/list?size=13")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.items);
+          setTotal(result.total)
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="home-container">
       <h1>Home Page</h1>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            {item.name} {item.price}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+    
+    );
+  }
 }
 
 export default Home
